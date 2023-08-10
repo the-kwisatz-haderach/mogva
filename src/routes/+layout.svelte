9a7c55e90ba37020b999f9f4999c46deea8c70e8
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-08-10 17:21:41 +0200

    Add metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,11 @@
 </script>
 
 <svelte:head>
+	<title>Mogva AB | No-nonsense Fullstack Web Development</title>
+	<meta
+		name="description"
+		content="Specialised in building safe, scalable, reliable & user friendly applications for the web."
+	/>
 	<meta name="color-scheme" content={$theme == 'system' ? 'light dark' : $theme} />
 	<link rel="stylesheet" href={`/theme/${$theme}.css`} />
 </svelte:head>
