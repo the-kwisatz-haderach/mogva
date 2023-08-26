59af1afd9682ba0724ad74e0774b8e3fcebc4d9f
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-08-26 15:02:34 +0200

    Update About text

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -6,29 +6,37 @@
 <Container id="about" color="black-dark" maxWidth={1000}>
 	<div class="content-wrapper">
 		<div class="text-wrapper">
-			<Typography color="contrast" variant="h2">About</Typography>
+			<Typography color="contrast" variant="h2">About me</Typography>
 			<Typography color="contrast"
-				>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita velit nostrum deserunt
-				sequi cupiditate. Doloremque illo officia minus doloribus facilis.</Typography
+				>My path to becoming a software developer has not been all straight. I spent the first five
+				years of my career managing digital projects and teams. During this time, I noticed I was
+				the happiest getting my hands dirty with the nitty gritty parts of the development process,
+				and started dreaming of a new career.</Typography
 			>
 			<Typography color="contrast">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in non cupiditate maxime
-				debitis incidunt corporis animi, repudiandae quisquam, quae, laudantium placeat consectetur
-				voluptas maiores!
+				Not being scared of change, I switched from an already great position and incredible
+				colleagues to jump on the fast-paced SALT developer bootcamp.
 			</Typography>
 			<Typography color="contrast">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in non cupiditate maxime
-				debitis incidunt corporis animi, repudiandae quisquam, quae, laudantium placeat.
+				Since finishing, I've spent countless hours picking up new knowledge in the various domains
+				of software development, both on the job but naturally also on my free time - not because I
+				had to but because I truly love the work.
+			</Typography>
+			<Typography color="contrast"
+				>Today, I consider my unconventional journey a big strength. Being quick to see the bigger
+				picture, I will lean on my past experiences to improve teamwork and communication, engage
+				relevant stakeholders early on, and am not afraid to express new ideas or feedback on
+				established norms.
 			</Typography>
 		</div>
-		<div class="image-wrapper">
+		<!-- <div class="image-wrapper">
 			<img
 				width="100%"
 				height="100%"
 				src="https://www.freeiconspng.com/thumbs/batman-png/batman-png-32.png"
 				alt=""
 			/>
-		</div>
+		</div> -->
 	</div>
 </Container>
 
@@ -38,7 +46,7 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		margin-bottom: -5rem;
+		// margin-bottom: -5rem;
 	}
 
 	.image-wrapper {
